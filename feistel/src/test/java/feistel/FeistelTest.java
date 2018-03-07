dec7d0f8be1190da70355a62efc37f8e237f6c1d
Lae <laec@protonmail.ch>
2018-03-07 19:47:46 +1300

    Move file.

--- feistel/src/test/java/feistel/FeistelTest.java
+++ feistel/src/test/java/feistel/FeistelTest.java
@@ -3,18 +3,32 @@ package feistel;
 import org.junit.Test;
 
 import java.util.function.IntUnaryOperator;
+import java.util.stream.IntStream;
 
+import static feistel.Feistel.compute;
 import static org.junit.Assert.assertEquals;
 
 public final class FeistelTest {
 
     @Test
+    public void isPermutation() {
+        IntUnaryOperator feistel = compute(3, (value) -> value * 11);
+        int step = 7231;
+        long count = (1L << 32) / step;
+        assertEquals(count, IntStream
+                .iterate(Integer.MIN_VALUE, i -> i + step)
+                .limit(count)
+                .map(feistel)
+                .distinct()
+                .count());
+    }
+
+    @Test
     public void canBeReversed() {
-        IntUnaryOperator f1 = Feistel.compute(7, (value) -> value * 12345);
-        IntUnaryOperator f2 = Feistel.compute(8, (value) -> value * 54321);
-        for (int i = Short.MIN_VALUE; i <= Short.MAX_VALUE; i++) {
-            assertEquals(i, f1.applyAsInt(f1.applyAsInt(i)));
-            assertEquals(i, f2.applyAsInt(f2.applyAsInt(i)));
+        IntUnaryOperator feistel = compute(4, (value) -> value * 31);
+        IntUnaryOperator inverse = feistel.compose(feistel);
+        for (long i = Integer.MIN_VALUE; i <= Integer.MAX_VALUE; i += 321) {
+            assertEquals(i, inverse.applyAsInt((int) i));
         }
     }
 
