1f42dd1a5106a8819dc766b096b21e8ecfe0fc4a
Lae <laec@protonmail.ch>
2018-03-17 09:22:12 +1300

    Refactor.

--- feistel/src/test/java/feistel/FeistelBigIntegerNumericTest.java
+++ feistel/src/test/java/feistel/FeistelBigIntegerNumericTest.java
@@ -18,35 +18,52 @@ final class FeistelBigIntegerNumericTest {
         BigInteger _31 = BigInteger.valueOf(31);
         RoundFunction<BigInteger> f = (r, v) -> v.multiply(_31).shiftLeft(r);
         return Stream.of(
-                new FeistelBigIntegerNumeric1(0, big(0), big(0), false, f),
-                new FeistelBigIntegerNumeric2(0, big(0), big(0), false, f),
-                new FeistelBigIntegerNumeric1(0, big(0), big(1), false, f),
-                new FeistelBigIntegerNumeric2(0, big(0), big(1), false, f),
-                new FeistelBigIntegerNumeric1(0, big(1), big(1), false, f),
-                new FeistelBigIntegerNumeric2(0, big(1), big(1), false, f),
-                new FeistelBigIntegerNumeric1(1, big(0), big(0), false, f),
-                new FeistelBigIntegerNumeric2(1, big(0), big(0), false, f),
-                new FeistelBigIntegerNumeric1(1, big(0), big(1), false, f),
-                new FeistelBigIntegerNumeric2(1, big(0), big(1), false, f),
-                new FeistelBigIntegerNumeric1(1, big(1), big(1), false, f),
-                new FeistelBigIntegerNumeric2(1, big(1), big(1), false, f),
-                new FeistelBigIntegerNumeric1(11, big(1), big(1), false, f),
-                new FeistelBigIntegerNumeric2(11, big(1), big(1), false, f),
-                new FeistelBigIntegerNumeric1(7, big(0), big(200), false, f),
-                new FeistelBigIntegerNumeric2(7, big(0), big(200), false, f),
-                new FeistelBigIntegerNumeric1(7, big(320), big(0), false, f),
-                new FeistelBigIntegerNumeric2(7, big(320), big(0), false, f),
-                new FeistelBigIntegerNumeric1(7, big(320), big(200), false, f),
-                new FeistelBigIntegerNumeric2(7, big(320), big(200), false, f),
-                new FeistelBigIntegerNumeric1(11, big(320), big(200), false, f),
-                new FeistelBigIntegerNumeric2(11, big(320), big(200), false, f),
-                new FeistelBigIntegerNumeric1(12, big(99), big(99), false, f),
-                new FeistelBigIntegerNumeric2(12, big(99), big(99), false, f)
-        );
+                new Params(0, 0, 0, f),
+                new Params(0, 0, 0, f),
+                new Params(0, 0, 1, f),
+                new Params(0, 0, 1, f),
+                new Params(0, 1, 1, f),
+                new Params(0, 1, 1, f),
+                new Params(1, 0, 0, f),
+                new Params(1, 0, 0, f),
+                new Params(1, 0, 1, f),
+                new Params(1, 0, 1, f),
+                new Params(1, 1, 1, f),
+                new Params(1, 1, 1, f),
+                new Params(11, 1, 1, f),
+                new Params(11, 1, 1, f),
+                new Params(7, 0, 200, f),
+                new Params(7, 0, 200, f),
+                new Params(7, 320, 0, f),
+                new Params(7, 320, 0, f),
+                new Params(7, 320, 200, f),
+                new Params(7, 320, 200, f),
+                new Params(11, 320, 200, f),
+                new Params(11, 320, 200, f),
+                new Params(12, 99, 99, f),
+                new Params(12, 99, 99, f)
+        ).flatMap(p -> {
+            BigInteger a = BigInteger.valueOf(p.a);
+            BigInteger b = BigInteger.valueOf(p.b);
+            return Stream.of(
+                    new FeistelBigIntegerNumeric1(p.round, a, b, false, p.f),
+                    new FeistelBigIntegerNumeric2(p.round, a, b, false, p.f)
+            );
+        });
     }
 
-    private static BigInteger big(long i) {
-        return BigInteger.valueOf(i);
+    private static final class Params {
+        final int round;
+        final long a;
+        final long b;
+        final RoundFunction<BigInteger> f;
+
+        Params(int round, long a, long b, RoundFunction<BigInteger> f) {
+            this.round = round;
+            this.a = a;
+            this.b = b;
+            this.f = f;
+        }
     }
 
     @ParameterizedTest
